93bef31b4522980e9d61d979fd64803c7254adf7
biaobiao <1191218051@qq.com>
2023-07-16 10:40:45 +0800

    二重密码

---
+++ 0716.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int main()
+{
+int m,n;
+
+
+scanf("%d",&n);
+
+    switch(n)
+   {
+    case 520:
+    {
+
+  scanf("%d",&m);
+
+  
+   switch(m)
+     {
+  
+    case 1314:
+    printf("恭喜");
+    
+    
+break;
+    default:
+    printf("出错了");
+    break;
+    }
+     }
+    
+    
+    break;
+    
+    default:
+    printf("出错了");
+   break;
+
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+  
+  
+}
+    return 0;
+}
